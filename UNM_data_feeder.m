0eb30075bbdfde07e3276a3f78dfeeaec1cbfc71
Timothy W. Hilton <hilton@unm.edu>
2011-08-25 17:32:31 -0600

    removed isintval from UNM_data_feeder.m, made it its own function.

--- UNM_data_feeder.m
+++ UNM_data_feeder.m
@@ -223,17 +223,3 @@ function [result] = valid_jdate(y, jd)
              jd >= 1;
 
 
-%----------
-% check whether a variable has an integer value
-% this is different from isinteger(), which checks the *class*.  A
-% variable of class double with an integral value will pass here.
-% INPUTS:
-%   x: matlab object
-% OUTPUTS:
-%   result: logical; true if x contains an integral value
-%
-%Timothy W. Hilton, UNM, August 2011
-     
-function [result] = isintval(x)
-    result = isnumeric(x) && mod(x, 1) == 0;
-
