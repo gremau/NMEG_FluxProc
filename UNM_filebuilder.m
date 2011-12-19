9d2676a48fa514ed0d3d8e19f57d2e9ae4ec76e4
Timothy W. Hilton <hilton@unm.edu>
2011-12-19 16:43:31 -0500

    fixed file name error

--- UNM_filebuilder.m
+++ UNM_filebuilder.m
@@ -60,7 +60,7 @@ outfolder=strcat(drive,'\','Research - Flux Towers\','Flux Tower Data by Site\',
                  site,'\matlab output\');
 sitedir=strcat(drive,'\','Research - Flux Towers\','Flux Tower Data by Site\', ...
                site,'\');
-fn_beg='TOB1_1300.ts_data'; %beginning of filename (before site & date)
+fn_beg='TOB1_1300.ts_data_'; %beginning of filename (before site & date)
 %fn_end='_0000_TOB1_TS_DATA.TOB';    %.DAT needed sometimes  %end of file name. make sure this matches.  (fn_end='_0000_TOB1_TS_data.TOB');
 fn_end='_0000.DAT';
 
@@ -139,16 +139,16 @@ end
 
 if dircode==0;
     if i==1
-        filename=strcat(fn_beg,site,'_',year_s,'_',month_s,'_',date_s,'_',starttime); %, '.DAT'
+        filename=strcat(fn_beg,'_',year_s,'_',month_s,'_',date_s,'_',starttime); %, '.DAT'
     else
-        filename=strcat(fn_beg,site,'_',year_s,'_',month_s,'_',date_s,fn_end);
+        filename=strcat(fn_beg,'_',year_s,'_',month_s,'_',date_s,fn_end);
     end
 elseif dircode==1;
     if i==1
-        filename = strcat(filedir,fn_beg,site,'_',year_s,'_',month_s,'_',date_s,'_',starttime) %, '.DAT'
+        filename = strcat(filedir,fn_beg,'_',year_s,'_',month_s,'_',date_s,'_',starttime) %, '.DAT'
        %  filename=strcat(filedir, fn_beg,year_s,'_',month_s,'_',date_s,'_',starttime) %, '.DAT'
     else
-        filename = strcat(filedir,fn_beg, site,'_', year_s,'_',month_s,'_',date_s,fn_end);
+        filename = strcat(filedir,fn_beg,'_', year_s,'_',month_s,'_',date_s,fn_end);
       %  filename=strcat(filedir,fn_beg, year_s,'_',month_s,'_',date_s,fn_end)
     end
 end
