aae5a3ebcab3d333f1689ec44a225d57658cf38c
Timothy W. Hilton <hilton@unm.edu>
2011-12-21 13:10:54 -0700

    implemented & tested combine & fill for TOA5 data

--- combine_and_fill_TOA5_files.m
+++ combine_and_fill_TOA5_files.m
@@ -1,4 +1,10 @@
 function ds = combine_and_fill_TOA5_files()
+% combine_and_fill_TOA5_files() --
+% prompts user to select a group of TOA5 files, combines them into one matlab
+% dataset and fills in any missing 30-minute time stamps.  Returns the filled
+% TOA5 data as a matlab datset.
+%
+% Timothy W. Hilton, UNM, Dec 2011
 
     [filename, pathname, filterindex] = uigetfile( ...
         { 'TOA5*.dat','TOA5 files (TOA5*.dat)' }, ...
@@ -8,13 +14,29 @@ function ds = combine_and_fill_TOA5_files()
         'MultiSelect', 'on' );
     
     nfiles = length( filename );
-    ds_array = cell( nfiles );
+    ds_array = cell( nfiles, 1 );
     
-    for i in 1:nfiles
-        fprintf( 1, 'reading %s\n', filename( i ) );
-        ds_array{ i } = toa5_to_dataset( fullfile( pathname, filename ) );
+    for i = 1:nfiles
+        fprintf( 1, 'reading %s\n', filename{ i } );
+        ds_array{ i } = toa5_2_dataset( fullfile( pathname, filename{ i } ) );
     end
     
-    ds = ds_array;
+    % combine ds_array to single dataset
+    ds = vertcat( ds_array{ : } );
+
+    fprintf( 1, 'filling missing timestamps\n' );
+    thirty_mins = 1 / 48;  %thirty minutes expressed in units of days
+    ds = dataset_fill_timestamps( ds, 'TIMESTAMP', thirty_mins );
+
+    % remove duplicated timestamps (e.g., in TX 2010)
+    fprintf( 1, 'removing duplicate timestamps\n' );
+    ts = datenum( ds.TIMESTAMP( : ) );
+    one_minute = 1 / ( 60 * 24 ); %one minute expressed in units of days
+    non_dup_idx = find( diff( ts ) > one_minute );
+    ds = ds( non_dup_idx, : );
     
     
+    % to save to file, use e.g.:
+    fprintf( 1, 'saving csv file\n' );
+    idx = min( find( datenum(ds.TIMESTAMP(:))>= datenum( 2010, 1, 1)));
+    export(ds( idx:end, : ), 'FILE', 'test.csv', 'Delimiter', ',');
