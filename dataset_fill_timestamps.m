029b8d0f1b2bc97494e66184ed47c4ca1fc7623f
Timothy W. Hilton <hilton@unm.edu>
2012-01-18 14:49:42 -0700

    added options to  dataset_fill_timestamps to specify the range in which to fill series

--- dataset_fill_timestamps.m
+++ dataset_fill_timestamps.m
@@ -1,16 +1,22 @@
-function ds_filled = dataset_fill_timestamps(ds, t_var, delta_t)
-% DATASET_FILL_TIMESTAMPS - fill in missing timestamps in a dataset containing a
-% regularly-spaced time series
-% INPUTS:
-%   ds: dataset to be filled
-%   t_var: string containing the name of the time variable (e.g. 'TIMESTAMP')
-%   delta_t: interval of the time series, in days.  e.g., 30 mins should have
-%            delta_t value of 1/48.
-%
-% Timothy W. Hilton, UNM, Dec. 2011
+function ds_filled = dataset_fill_timestamps( ds, t_var, delta_t, ...
+                                              t_min, t_max )
+    
+    % DATASET_FILL_TIMESTAMPS - fill in missing timestamps in a dataset containing a
+    % regularly-spaced time series
+    % INPUTS:
+    %   ds: dataset to be filled
+    %   t_var: string containing the name of the time variable (e.g. 'TIMESTAMP')
+    %   delta_t: interval of the time series, in days.  e.g., 30 mins should have
+    %            delta_t value of 1/48.
+    %
+    % Timothy W. Hilton, UNM, Dec. 2011
 
-    t_min = min( ds.( t_var ) );
-    t_max = max( ds.( t_var ) );
+    if isnan( t_min )
+        t_min = min( ds.( t_var ) );
+    end
+    if isnan( t_max )
+        t_max = max( ds.( t_var ) );
+    end
 
     full_ts = ( t_min : delta_t : t_max )';
     full_ts = cellstr( datestr( full_ts, 'mm/dd/yyyy HH:MM:SS' ) );
@@ -31,7 +37,7 @@ function ds_filled = dataset_fill_timestamps(ds, t_var, delta_t)
 
     % timestamps (they're strings now) got sorted lexigrapically -- sort
     % them now by the actual date
-    dn = datenum(ds_filled.TIMESTAMP, 'mm/dd/yyyy HH:MM:SS');
+    dn = datenum(ds_filled.( t_var ), 'mm/dd/yyyy HH:MM:SS');
     [ discard, idx ] = sort( dn );
     ds_filled = ds_filled( idx, : );
     
