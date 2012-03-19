d024c9d902520ed045d5bdab15d3e4e5ce1c9842
Timothy W. Hilton <hilton@unm.edu>
2012-03-19 17:02:04 -0600

    added optional plotting to filling T, RH from nearby sites

--- UNM_fill_met_gaps_from_nearby_site.m
+++ UNM_fill_met_gaps_from_nearby_site.m
@@ -1,4 +1,4 @@
-function result = UNM_fill_met_gaps_from_nearby_site( site_code, year )
+function result = UNM_fill_met_gaps_from_nearby_site( site_code, year, draw_plots )
 % UNM_FILL_MET_GAPS_FROM_NEARBY_SITE - fills gaps in site's meteorological data
 %   from the closest nearby site
 %
@@ -36,6 +36,9 @@ fprintf( 'parsing %s_flux_all_%d_for_gapfilling.txt ("destination")\n', ...
          get_site_name( site_code ), year );
 this_data = parse_forgapfilling_file( site_code, year );
 
+%--------------------------------------------------
+% fill T, plot if requested
+
 % replace missing Tair with nearby site
 Tair_idx = isnan( this_data.Tair );
 n_valid_fillins = numel( find( ~isnan( nearby_data.Tair( Tair_idx ) ) ) );
@@ -43,7 +46,23 @@ this_data.Tair( Tair_idx ) = nearby_data.Tair( Tair_idx );
 fprintf( 'Tair: replaced %d / %d missing observations\n', ...
          n_valid_fillins, ...
          numel( find( Tair_idx ) ) );
-                
+
+if draw_plots
+    % calculate day of year for 30-minute data
+    h_T_fig = figure();
+    doy = ( 1:size( this_data, 1 ) ) / 48.0;
+    h_obs = plot( doy, this_data.Tair, '.k' );
+    hold on;
+    h_filled = plot( doy( find( Tair_idx ) ), nearby_data.Tair( find( Tair_idx ) ), ...
+                     '.', 'MarkerEdgeColor', [ 27, 158, 119 ] / 255.0 );
+    ylabel( 'Tair (C)' );
+    xlabel( 'day of year' );
+    legend( [ h_obs, h_filled ], 'observed', 'filled' );
+end
+
+%--------------------------------------------------
+% fill RH, plot if requested
+
 % replace missing RH with nearby site
 rH_idx = isnan( this_data.rH );
 n_valid_fillins = numel( find( ~isnan( nearby_data.rH( rH_idx ) ) ) );
@@ -52,5 +71,18 @@ fprintf( 'rH: replaced %d / %d missing observations\n', ...
          n_valid_fillins, ...
          numel( find( rH_idx ) ) );
 fprintf( '-----\n' );
+
+if draw_plots
+    % calculate day of year for 30-minute data
+    h_RH_fig = figure();
+    doy = ( 1:size( this_data, 1 ) ) / 48.0;
+    h_obs = plot( doy, this_data.rH, '.k' );
+    hold on;
+    h_filled = plot( doy( find( rH_idx ) ), nearby_data.rH( find( rH_idx ) ), ...
+                     '.', 'MarkerEdgeColor', [ 27, 158, 119 ] / 255.0 );
+    ylabel( 'RH (%)' );
+    xlabel( 'day of year' );
+    legend( [ h_obs, h_filled ], 'observed', 'filled' );
+end
 
 result = this_data;
