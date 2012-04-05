e9bac65b6abdd13c109507603fc8200694815191
Timothy W. Hilton <hilton@unm.edu>
2012-04-05 09:20:49 -0600

    tried to fix UNM_online_gapfiller_submit_job.m; still not working

--- script_removebaddata.m
+++ script_removebaddata.m
@@ -1,7 +1,9 @@
-sites = [ 2, 3, 4, 5, 6, 10, 11];
+sites = [ 1, 2, 3, 4, 5, 6, 10 ];
 
 for i = 1:numel( sites )
     for y = 2009:2010
         UNM_RemoveBadData( sites( i ), y );
     end
 end
+
+UNM_RemoveBadData( 11, 2010 );
