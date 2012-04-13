30563eaefb1a9d05f5cd63a15b7ec3234248768c
Timothy W. Hilton <hilton@unm.edu>
2012-04-13 10:45:27 -0600

    edited parse_ameriflux_file to automatically detect the delimiter

--- parse_ameriflux_file.m
+++ parse_ameriflux_file.m
@@ -7,7 +7,7 @@ function amflux_ds = parse_ameriflux_file( fname )
 % (c) Timothy W. Hilton, UNM, Dec 2011
 
 headerlines = 3;
-delim = ',';
+delim = detect_delimiter( fname )
 
 fid = fopen( fname, 'r' );
 
