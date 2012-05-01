27f527629db34704f8b16b66bfff06ca2f5abd95
Timothy W. Hilton <hilton@unm.edu>
2012-05-01 17:29:49 -0600

    added column index output to regexp_ds_vars

--- regexp_ds_vars.m
+++ regexp_ds_vars.m
@@ -1,18 +1,23 @@
-function vars = regexp_ds_vars( ds, re )
+function [ vars, varargout ] = regexp_ds_vars( ds, re )
 % REGEXP_DS_VARS - returns cell array of strings of variable names from dataset
 %   ds that match regular expression re
 %
 % USAGE
 %    vars = regexp_ds_vars( ds, re )
+%    [ vars, idx ] = regexp_ds_vars( ds, re )
 %
 % INPUTS
 %    ds: Matlab dataset
+%    re: string; regular expression to match against dataset variable names
 %
 % OUTPUTS
-%    re: string; regular expression to match against dataset variable names
+%    vars: cell array of strings; variable names from ds that match re
+%    idx: optional; vector of column indices of the matches
 %
 % (c) Timothy W. Hilton, UNM, Jan 2012
     
     vars = regexp( ds.Properties.VarNames, re, 'match' );
-    vars = vars( ~cellfun( 'isempty', vars ) );
-    vars = cellfun( @cell2mat, vars, 'UniformOutput', false ); 
+    idx = find( ~cellfun( 'isempty', vars ) );
+    vars = ds.Properties.VarNames( idx );
+
+    varargout = { idx };
