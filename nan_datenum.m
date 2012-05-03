b0ca19cce3e8a22ab2807d71c2dd0a15a6cfb4bd
Timothy W. Hilton <hilton@unm.edu>
2012-05-03 09:12:51 -0600

    implemented nan_datenum to handle character string dates with missing timestamps

---
+++ nan_datenum.m
@@ -0,0 +1,22 @@
+function dn = nan_datenum( str, fmt )
+% NAN_DATENUM - processes strings to matlab datenums, also handling missing
+% elements (NaNs).  Matlab's datenum function refuses to process NaNs if
+% given the optional format string as input.
+%   
+% USAGE
+%   dn = nan_datenum( str, fmt )
+%
+% INPUTS
+%   str: character array (representing a date), or NaN
+%   fmt: character array specifying date format (see datenum docs)
+%
+% OUTPUTS
+%   dn: if str represents a date, a matlab datenum.  If str is NaN, NaN.
+%
+% (c) Timothy W. Hilton, UNM, April 2012
+
+dn = NaN;
+
+if ~isempty( str )
+    dn = datenum( str, fmt );
+end
