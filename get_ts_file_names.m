f14bc7c3ea185cb7019222b452710e1e8b12e11c
Timothy W. Hilton <hilton@unm.edu>
2012-05-23 11:25:41 -0600

    now ignores files with no proper timestamp in file name to solve 'datenum empty string' warnings

--- get_ts_file_names.m
+++ get_ts_file_names.m
@@ -1,7 +1,20 @@
-function fnames = get_ts_file_names(site, date_start, date_end)
+function fnames = get_ts_file_names( site, date_start, date_end )
 % GET_TS_FILE_NAMES - returns the full paths of all TOB1 time series data files
 %   in the site's directory that contain data recorded between start_date and
 %   end_date
+% 
+% USAGE
+%   fnames = get_ts_file_names( site, date_start, date_end )
+%
+% INPUTS
+%   site: site name *string* -- e.g. GLand, MCon, etc.
+%   date_start: matlab datenum
+%   date_end: matlab datenum
+%
+% OUTPUTS
+%   fnames: cell array of file names (with full paths)
+%
+% (c) Timothy W. Hilton, UNM
 
     data_dir = get_site_directory(get_site_code(site));
     if exist(data_dir) ~= 7
@@ -11,18 +24,23 @@ function fnames = get_ts_file_names(site, date_start, date_end)
         fnames = list_files( fullfile( data_dir, 'ts_data' ), ...
                              'TOB1.*(\.DAT$|\.dat$)');
     end
-    
+
+    %-----
     %read the time stamps from the file names into matlab datenums
-    % prefix = sprintf( 'TOB1_%s_', site );
-    % tstamp_strings = strrep({fnames.name}, prefix, '');
-    % tstamp_strings = regexprep(tstamp_strings, '\.DAT', '', 'ignorecase');
     tstamp_strings = regexp( fnames, ...
                              '\d\d\d\d_\d\d_\d\d_\d\d\d\d', ...
-                             'match' );
+                             'match', 'once' );
+    
+    % ignore files with no properly-formatted timestamp in the file name
+    empty_idx = find( cellfun( @isempty, tstamp_strings ) );
+    tstamp_strings( empty_idx ) = [];
+    fnames( empty_idx ) = [];
+    
     dn = cellfun( @( x ) datenum(x, 'yyyy_mm_dd_HHMM'), ...
                   tstamp_strings, ...
                   'UniformOutput', false );
     dn = [ dn{:} ];
+    %-----
     
     % find and return file names from the requested date range
     idx = find(dn >= date_start & dn <= date_end);
