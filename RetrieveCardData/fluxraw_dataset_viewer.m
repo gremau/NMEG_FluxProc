a1b6184d42f6d3b219310ed94f490d231fa34907
Timothy W. Hilton <hilton@unm.edu>
2012-06-05 14:35:54 -0600

    implementing better viewer for card data

---
+++ RetrieveCardData/fluxraw_dataset_viewer.m
@@ -0,0 +1,166 @@
+function fh =  fluxraw_dataset_viewer( ds )
+% dataset_viewer -- a graphical user interface to view matlab variables of
+% class dataset column by column
+%       
+% USAGE
+%    fh = datasetviewer( ds )
+%
+% INPUTS:
+%    ds: variable of class dataset
+%
+% OUTPUTS:
+%
+%    fh: handle to the figure window created
+%
+% (c) Timothy W. Hilton, UNM, Mar 2012
+    
+%-------------------------
+% Initialization tasks
+%--------------------------
+
+% determine screensize
+    scrsz = get(0,'ScreenSize');
+    
+    nfields = size( ds, 2 );
+
+    % create a figure to contain the GUI, use entire screen horizontally,
+    % upper half of the screen vertically
+    fh = figure( 'Name', 'dataset viewer', ...
+                 'Position', scrsz ./ [ 1, 2, 1, 2 ], ...  
+                 'NumberTitle', 'off', ...
+                 'ToolBar', 'figure', ...
+                 'MenuBar', 'none' );
+    set( fh, 'UserData', struct( 'cur_col', 1 ) ); % start with column 1
+    
+    %--------------------------
+    % Construct GUI components
+    %--------------------------
+    
+    %add axes for plots
+    axh = axes( 'Parent', fh, ...
+                'Units', 'normalized', ...
+                'HandleVisibility','callback', ...
+                'Position',[ 0.1, 0.15, 0.8, 0.7 ] );
+    
+    %add a "previous" button
+    pbh_prev = uicontrol( fh, ...
+                          'Style', 'pushbutton', ...
+                          'String','previous', ...
+                          'Position', [ 50 10 200 40] );
+    set( pbh_prev, 'enable', 'off' );  % start at the first column
+    
+    %add a "next" button
+    pbh_next = uicontrol( fh, ...
+                          'Style', 'pushbutton', ...
+                          'String','next', ...
+                          'Position', [ 250 10 200 40], ...
+                          'CallBack', { @next_but_cbk, nfields, ...
+                        axh, fh, ds, pbh_prev } );
+
+    set( pbh_prev, 'CallBack', { @prev_but_cbk, nfields, axh, fh, ds, pbh_next} );
+    set( pbh_next, 'CallBack', { @next_but_cbk, nfields, axh, fh, ds, pbh_prev} );
+
+    pbh_zoom = uicontrol( fh, ...
+                          'Style', 'pushbutton', ...
+                          'String','Zoom', ...
+                          'Position', [ 650 10 200 40], ...
+                          'CallBack', { @zoom_but_cbk, nfields, ...
+                        axh, fh, ds } );
+    set( pbh_zoom, 'CallBack', { @zoom_but_cbk, nfields, axh, fh, ds } );
+
+%--------------------------
+%  plot first field
+%--------------------------
+
+plot( axh, ds( :, 1 ), '.k' );
+xlabel( axh, 'index' );
+t_str = ds.Properties.VarNames{ 1 };
+t_str = strrep( t_str, '_', '\_');
+t_str = strrep( t_str, '0x2E', '.');
+ylabel( axh, t_str );
+
+%--------------------------
+%  Callbacks for GUI
+%--------------------------
+
+function cur_col = prev_but_cbk( source, eventdata, ...
+                                 nfields, axh, fh, ds, pbh_next )
+    %% change the plot to the previous data field
+    
+    % decrement cur_col
+    ud = get( fh, 'UserData' );
+    ud.cur_col = max( ud.cur_col - 1, 1 );
+    % get the new variable names
+    this_var = ds.Properties.VarNames{ ud.cur_col };
+    if isnumeric( ds.( this_var ) )
+        plot( axh, ds.( this_var ), '.k' );
+    else
+        cla( axh );
+    end
+    set( fh, 'UserData', ud );
+
+    % label x axis on lower plot
+    xlabel( axh, 'index' );
+    
+    % title string
+    t_str = strrep( this_var, '_', '\_');
+    t_str = strrep( t_str, '0x2E', '.');
+    ylabel( axh, t_str );
+    
+    % just backed up, so can't be on last column
+    set( pbh_next, 'enable', 'on' );
+    
+    % disable "prev" button if we're on the first column
+    if ud.cur_col == 1
+        set( source, 'enable', 'off' );
+    else
+        set( source, 'enable', 'on' );
+    end    
+    
+function cur_col = next_but_cbk( source, eventdata, ...
+                                 nfields, axh, fh, ds, pbh_prev )
+    %% change the plot to the previous data field
+    
+    % decrement cur_col
+    ud = get( fh, 'UserData' );
+    ud.cur_col = min( ud.cur_col + 1, nfields );
+    % get the new variable names
+    this_var = ds.Properties.VarNames{ ud.cur_col };
+        if isnumeric( ds.( this_var ) )
+        plot( axh, ds.timestamp, ds.( this_var ), '.k' );
+        this_units = ds.Properties.Units{ ud.cur_col };
+    else
+        cla( axh );
+    end
+
+    set( fh, 'UserData', ud );
+
+    % label x axis on lower plot
+    xlabel( axh, 'date' );
+    datetick( 'x', 'dd-mmm-yy' );
+    
+    % title string
+    t_str = strrep( this_var, '_', '\_');
+    t_str = strrep( t_str, '0x2E', '.');
+    ylabel( axh, sprintf( '%s [%s]', t_str, this_units ) );
+
+    % just advanced, so can't be on first column
+    set( pbh_prev, 'enable', 'on' );
+    
+    % disable "next" button if we're on the last column
+    if ud.cur_col == nfields
+        set( source, 'enable', 'off' );
+    else
+        set( source, 'enable', 'on' );
+    end
+    
+function zoom_but_cbk( source, eventdata, ...
+                       nfields, axh, fh, ds, pbh_prev )
+%% change the plot to the previous data field
+
+ud = get( fh, 'UserData' );
+cur_ylim = get( axh, 'YLim' );
+
+new_ylim = prctile( double( ds( :, ud.cur_col ) ), ...
+                    [ 0.1, 99.9 ] );
+set( axh, 'YLim', new_ylim );
