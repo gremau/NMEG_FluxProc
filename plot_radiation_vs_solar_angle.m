96a32060c32a7668962cdfb01b2335b5158ae735
Timothy W. Hilton <hilton@unm.edu>
2012-06-15 11:39:44 -0600

    changed horizontal axis to DTIME (from index) in solar angle plots

--- plot_radiation_vs_solar_angle.m
+++ plot_radiation_vs_solar_angle.m
@@ -4,7 +4,7 @@ sd = parse_UNM_site_table();
 
 fname = get_ameriflux_filename( sitecode, ...
                                 year, ...
-                                'gapfilled' )
+                                'gapfilled' );
 this_data = parse_ameriflux_file( fname );
 this_data.timestamp = datenum( year, 1, 0 ) + this_data.DTIME;
 this_data.SolEl = get_solar_elevation( sitecode, this_data.timestamp );
@@ -12,18 +12,22 @@ this_data.SolEl = get_solar_elevation( sitecode, this_data.timestamp );
 pal = brewer_palettes( 'Dark2' );
 t_str = sprintf( '%s %d', ...
                  sd.SITE_NAME{ sitecode }, ...
-                 year )
+                 year );
 this_fig = figure( 'NumberTitle', 'off', ...
                    'Name', t_str );
-h_all = plot( this_data.Rg, '.k' );
+h_all = plot( this_data.DTIME, this_data.Rg, '.k' );
 ylabel( 'Rg' );
 xlabel( 'index' );
 title( t_str );
-hold on
+hold on;
 idx = find( this_data.SolEl < 0 );
-h_sundown = plot( idx, this_data.Rg( idx ), '.', 'Color', pal( 1, : ) );
+h_sundown = plot( this_data.DTIME( idx ), ...
+                  this_data.Rg( idx ), ...
+                  '.', 'Color', pal( 1, : ) );
 idx = find( this_data.Rg_flag );
-h_filled = plot( idx, this_data.Rg( idx ), 'o', 'Color', pal( 2, : ) );
+h_filled = plot( this_data.DTIME( idx ), ...
+                 this_data.Rg( idx ), ...
+                 'o', 'Color', pal( 2, : ) );
 h_50 = refline( 0, 50 );
 set( h_50, 'LineWidth', 2 );
 
