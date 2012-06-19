3c1790e74cca396b762c34075fbe80c48a2ffa15
Timothy W. Hilton <hilton@unm.edu>
2012-06-19 12:08:00 -0600

    small updates in fingerprint plotter

--- plot_fingerprint.m
+++ plot_fingerprint.m
@@ -12,9 +12,10 @@ hours_of_day = (1:48) / 2;
 
 image( hours_of_day, 1:max( doy ), data_rect, ...
        'CDataMapping', 'scaled');
-set( h_ax, 'YDir', 'normal' );
+set( h_ax, 'YDir', 'normal', 'XMinorTick', 'On' );
 pal = colormap( cbrewer( 'seq', 'YlOrRd', 9 ) );
-new_pal = [ pal( 1:6, : ); interp1( 0:3, pal( 6:9, : ), 0:1/10:3 ) ];
+new_pal = [ interp1( 1:6, pal( 1:6, : ), linspace( 1, 6, 10 ) ); ...
+            interp1( 0:3, pal( 6:9, : ), linspace( 0, 3, 60 ) ) ];
 %new_pal = [ pal( 1:8, : ); interp1( 0:1, pal( 8:9, : ), 0:1/100:1 ) ];
 
 colormap( new_pal );
