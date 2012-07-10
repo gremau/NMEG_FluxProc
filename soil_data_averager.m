899316dd4d3186dac10066a9a082afb32bb80c2d
Timothy W. Hilton <hilton@unm.edu>
2012-07-10 14:18:34 -0600

    added a prefix string to the output of soil_data_averager to label the variable that was averaged (Tsoil or VWC)

--- soil_data_averager.m
+++ soil_data_averager.m
@@ -7,9 +7,9 @@ function [ avg_soil_data, avg_by_cover ] = soil_data_averager( soil_data )
 %
 % INPUTS:
 %    soil_data: dataset; one column per soil observation.  Variables must be
-%    named with format obs_cover_idx_depth_*, where obs is the measurmement
-%    (e.g. 'soilT', 'VWC', etc., cover is the cover type ('open', 'pinon',
-%    etc.), depth is the depth (2p5, 12p5, etc.), and * is arbitrary text.
+%        named with format obs_cover_idx_depth_*, where obs is the measurmement
+%        (e.g. 'soilT', 'VWC', etc., cover is the cover type ('open', 'pinon',
+%        etc.), depth is the depth (2p5, 12p5, etc.), and * is arbitrary text.
 %
 % OUTPUTS:
 %    avg_soil_data: dataset containing average observations for each
@@ -21,9 +21,21 @@ function [ avg_soil_data, avg_by_cover ] = soil_data_averager( soil_data )
 grp_vars = regexp( soil_data.Properties.VarNames, '_', 'split' );
 grp_vars = vertcat( grp_vars{ : } ); 
 
+var = unique( grp_vars( :, 1 ) );  %variable (Tsoil, cs616) is 1st
+                                   %underscore-delimited field
 covers = unique( grp_vars( :, 2 ) );  %cover is 2nd underscore-delimited field
 depths = unique( grp_vars( :, 4 ) );  %depth is 4th underscore-delimited field
                 
+switch var{ 1 }
+  case 'cs616SWC'
+    prefix = 'VWC';
+  case 'soilT'
+    prefix = 'Tsoil';
+  otherwise
+    error( sprintf( [ 'label prefix should be either' ...
+                      '"cs616SWC" or "soilT".  It is "%s".' ], var ) );
+end
+
 % -----
 % calculate average at each depth by cover type
 % -----
@@ -37,7 +49,8 @@ soil_data = double( soil_data );
 count = 1;
 for this_cov = 1:numel( covers )
     for this_depth = 1:numel( depths )
-        avg_soil_data_vars{ count } = sprintf( '%s_%s', ...
+        avg_soil_data_vars{ count } = sprintf( '%s_%s_%s', ...
+                                               prefix, ...
                                                covers{ this_cov }, ...
                                                depths{ this_depth } );
         idx = strcmp( grp_vars( :, 4 ), depths( this_depth ) ) & ...
@@ -62,8 +75,8 @@ for this_cov = 1:numel( covers )
 
     avg_by_cover( :, this_cov ) = nanmean( soil_data( :, idx ), 2 );
 
-    avg_by_cover_vars{ this_cov } = sprintf( 'VWC_%s_Avg', ...
-                                             covers{ this_cov } );
+    avg_by_cover_vars{ this_cov } = sprintf( '%s_%s_Avg', ...
+                                             prefix, covers{ this_cov } );
 end
 
 avg_by_cover = dataset( { avg_by_cover, avg_by_cover_vars{ : } } );
