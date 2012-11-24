3bb5e5987ca5e900a92380587dcdfe9eac53b2c0
Timothy W. Hilton <hilton@unm.edu>
2012-11-23 20:58:36 -0700

    wrote code to deal with JSav CR1000 files

--- RetrieveCardData/JSavCR1000_2_TOA5.m
+++ RetrieveCardData/JSavCR1000_2_TOA5.m
@@ -1,13 +1,12 @@
-function [success, toa5_fname] = thirty_min_2_TOA5(site, raw_data_dir)
-% THIRTY_MIN_2_TOA5 - convert the thirty minute file (*.flux.dat) to a TOA5
-% file
+function [success, toa5_fname] = JSavCR1000_2_TOA5( raw_data_dir)
+% JSavCR1000_2_TOA5 - convert the soil water content from the CR1000 datalogger
+% at JSav to a TOA5 file
 
-site = UNM_sites( site );
+site = UNM_sites.JSav;
 
 success = true;
 
-thirty_min_file = dir(fullfile(raw_data_dir, '*.flux.dat'));
-ts_data_file = dir(fullfile(raw_data_dir, '*.ts_data.dat'));
+thirty_min_file = dir(fullfile(raw_data_dir, '*.soilwcr.dat'));
 toa5_data_dir = fullfile(get_site_directory( site ), 'toa5');
 if isempty(thirty_min_file)
     error('There is no thirty-minute data file in the given directory');
@@ -29,54 +28,22 @@ else
                                card_convert_exe, ...
                                toa5_ccf_file);
 
-    if not( isempty( ts_data_file ) )
-        % card convert will try to apply the ccf file to every .dat file in
-        % the directory.  We want to use a different ccf file for the TS
-        % data, so temporarily change the .dat extension so CardConvert will
-        % ignore it for now.
-        ts_file_ignore = fullfile(raw_data_dir, ...
-                                  strrep(ts_data_file.name, '.dat', '.donotconvert'));
-        sprintf('%s --> %s\n', fullfile(raw_data_dir, ts_data_file.name), ...
-                ts_file_ignore);
-        ts_file_fullpath = fullfile(raw_data_dir, ts_data_file.name);        
-        % matlab's movefile takes minutes to rename a 2 GB ts data file.  The
-        % java method does it instantly though
-        move_success = ...
-            java.io.File(ts_file_fullpath).renameTo(java.io.File(ts_file_ignore));
-        success = success & move_success;
-        if not(move_success)
-            error('thirty_min_2_TOA5:rename_fail', 'renaming TS data file failed');
-        end
-    end
-
-    
     % run CardConvert
     [convert_status, result] = system(card_convert_cmd);
     success = success & (convert_status == 0);
     if convert_status ~= 0
         error('thirty_min_2_TOA5:CardConvert', 'CardConvert failed');
     end
-
-    if not( isempty( ts_data_file ) )
-        %restore the .dat extension on the TS data file
-        move_success = ...
-            java.io.File(ts_file_ignore).renameTo(java.io.File(ts_file_fullpath));
-        success = success & move_success;
-        if not(move_success)
-            error('thirty_min_2_TOA5:rename_fail',...
-                  'restoring TS data .dat extension failed');
-        end
-    end
     
     %rename the TOA5 file according to the site and place it in the
     %site's TOA5 directory
     default_root = sprintf('TOA5_%s',...
                            char(regexp(thirty_min_file.name, ...
-                                       '.*\.flux', 'match')));
+                                       '.*\.soilwcr', 'match')));
     default_name = dir(fullfile(output_temp_dir, [default_root, '*']));
     newname = strrep(default_name.name, ...
                      default_root, ...
-                     sprintf('TOA5_%s', char(site)));
+                     'TOA5_JSav_soilwcr' );
     default_fullpath = fullfile(output_temp_dir, default_name.name);
     newname_fullpath = fullfile(toa5_data_dir, newname);
     if exist(newname_fullpath) == 2
