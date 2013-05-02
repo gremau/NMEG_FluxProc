2601e57e1606c5ebbba6b10d3f4073339c5448e6
Timothy W. Hilton <hilton@unm.edu>
2013-05-02 08:54:06 -0700

    set default plot MarkerSize to 12 for Mac

--- startup.m
+++ startup.m
@@ -3,5 +3,9 @@ if not( isempty( regexpi( computer(), 'mac' ) ) )
     % add Peter Acklam's time/date utilities to path
     addpath(genpath('/Users/tim/Software/Acklam_Utils/timeutil'));
     mac_setenv;
+    
+    % newer versions seem to use really tiny plotting markers by default --
+    % make them bigger
+    set(0,'DefaultLineMarkerSize',12);
 end
     
