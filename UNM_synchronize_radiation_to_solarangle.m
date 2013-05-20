3f7570e21aca4571d88be138ac91487db1383795
Timothy W. Hilton <hilton@unm.edu>
2013-05-20 10:37:41 -0600

    implemented code to fix radiation time offsets; needs debugging.  Also removed deprecated code from RBD_pre2012

---
+++ UNM_synchronize_radiation_to_solarangle.m
@@ -0,0 +1,60 @@
+function [ QC, fgf ] = ...
+    UNM_synchronize_radiation_to_solarangle( sitecode, year, ...
+                                             QC, fgf, timestamp )
+% UNM_SYNCHRONIZE_RADIATION_TO_SOLARANGLE - adjust data so that observed sunrise
+%   (as deterimined by Rg) corresponds to solar angle.
+
+sol_ang = UNM_get_solar_angle( sitecode, timestamp );
+
+n_days = 365;
+if isleapyear( year )
+    n_days = 366;
+end
+
+opt_off_Rg = repmat( NaN, 1, numel( 1:n_days ) );
+opt_off_PAR = repmat( NaN, 1, numel( 1:n_days ) );
+for doy = 1:n_days
+    opt_off_Rg( doy ) = match_solarangle_radiation( fgf.Rg, ...
+                                                    sol_ang, ...
+                                                    timestamp, ...
+                                                    doy, year, false );
+    opt_off_PAR( doy ) = match_solarangle_radiation( QC.Par_Avg, ...
+                                                     sol_ang, ...
+                                                     timestamp, ...
+                                                     doy, year, false );
+end
+
+% use Rg-based offset where available, fill in with PAR
+opt_off = opt_off_Rg;
+idx = isnan( opt_off );
+opt_off( idx ) = opt_off_PAR( idx );
+
+% convert QC, fgf to double arrays to apply the shift
+QC_names = QC.Properties.VarNames;
+fgf_names = fgf.Properties.VarNames;
+QC = double( QC );
+fgf = double( fgf );
+
+% use run length encoding to gather consecutive days with the same radiation
+% offset
+idx_rle = rle( opt_off );
+% the cumulative sums of the indices of the beginning of each change in
+% radiation offset provides the DOY that each period of equal offset (i.e.,
+% "chunk") begins
+DOY_chunk_start = cumsum( idx_rle{ 2 } );
+chunk_ndays = idx_rle{ 2 };
+chunk_offset = idx_rle{ 1 };
+for i = 1:numel( chunk_offset )
+    if not( isnan( chunk_offset( i ) ) )
+        idx0 = DOYidx( DOY_chunk_start( i ) );
+        idx1 = DOYidx( DOY_chunk_start( i ) + chunk_ndays( i ) );
+        QC( idx0:idx1, : ) = shift_data( QC( idx0:idx1, : ), ...
+                                         chunk_offset( i ) );
+        fgf( idx0:idx1, : ) = shift_data( fgf( idx0:idx1, : ), ...
+                                          chunk_offset( i ) );
+    end
+end
+
+% convert the shifted arrays back to dataset arrays
+QC = dataset( { QC, QC_names{ : } } );
+fgf = dataset( { fgf, fgf_names{ : } } );
