a0878ba184069e602008f1037fa2f1b888f8cf1a
Timothy W. Hilton <hilton@unm.edu>
2013-05-28 16:08:36 -0700

    implemented handling of dataset arrays

--- replace_badvals.m
+++ replace_badvals.m
@@ -5,7 +5,7 @@ function arr = replace_badvals(arr, badvals, tol)
 %     arr = replace_badvals( arr, badvals, tol )
 %
 % INPUTS
-%     arr: array in which to replace bad values
+%     arr: array or dataset array in which to replace bad values
 %     badvals: array of values to be replace with NaN
 %     tol: floating point tolerance for comparison
 %
@@ -14,12 +14,24 @@ function arr = replace_badvals(arr, badvals, tol)
 %
 % (c) Timothy W. Hilton, UNM
     
-    badvals = reshape(badvals, 1, numel(badvals));
-    badval_idx = zeros(size(arr));
+arg_is_dataset = false;
+if isa( arr, 'dataset' )
+    arg_is_dataset = true;
+    arr_arg = arr;
+    arr = double( arr );
+end
+
+badvals = reshape( badvals, 1, [] );
+badval_idx = zeros( size( arr ) );
+
+for i = 1:length(badvals)
     
-    for i = 1:length(badvals)
-        
-        badval_idx = badval_idx | abs(arr - badvals(i)) < tol;
-        arr(badval_idx) = NaN;
-        
-    end
+    badval_idx = badval_idx | abs(arr - badvals(i)) < tol;
+    arr(badval_idx) = NaN;
+    
+end
+
+% if argument was a dataset, convert it back
+if ( arg_is_dataset )
+    arr = replacedata( arr_arg, arr );
+end
