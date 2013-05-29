1c18e75cef7a10d4ea4dabbc02e167b929fa0ee2
Timothy W. Hilton <hilton@unm.edu>
2013-05-29 12:24:21 -0600

    added logging and more graceful error handling to card data retrieval

--- RetrieveCardData/process_card_main.m
+++ RetrieveCardData/process_card_main.m
@@ -9,6 +9,8 @@ function main_success = process_card_main( this_site, varargin )
 %    * copies uncompressed raw data to Story USB hard drive
 %    * compresses the raw data on the internal hard drive
 %    * FTPs the compressed raw data to EDAC
+%    * captures all output, error messages, etc. to a log file in
+%      $FLUXROOT/Card_Processing_Logs 
 %
 % USAGE: 
 %    process_card_main( this_site )
@@ -36,7 +38,7 @@ function main_success = process_card_main( this_site, varargin )
 %    is a garbled file on a card so that process_card_main crashes, or if the
 %    network connection dies so that FTP transfer does not complete.
 %
-% Timothy W. Hilton, UNM, Dec 2011
+% Timothy W. Hilton, UNM, 2011-2013
 
 % -----
 % define optional inputs, with defaults and typechecking
@@ -53,24 +55,51 @@ args.parse( this_site, varargin{ : } );
 this_site = args.Results.this_site;
 
 %--------------------------------------------------------------------------
+% open a log file
+
+fname_log = fullfile( getenv( 'FLUXROOT' ), ...
+                      'Card_Processing_Logs', ...
+                      sprintf( '%s_%s_card_process.log', ...
+                               datestr(now(), 'yyyy-mm-dd_HHMM' ), ...
+                               char( UNM_sites( this_site ) ) ) );
+diary( fname_log );
+
+%--------------------------------------------------------------------------
 site_dir = get_site_directory( this_site );
 
 % copy the data from the card to the computer's hard drive
-fprintf(1, '\n----------\n');
-fprintf(1, 'COPYING FROM CARD TO LOCAL DISK...\n');
-data_location = args.Results.data_location;
-if not( strcmp( data_location, 'card' ) )
-    data_location = args.Results.data_path;
+try    
+    fprintf(1, '\n----------\n');
+    fprintf(1, 'COPYING FROM CARD TO LOCAL DISK...\n');
+    data_location = args.Results.data_location;
+    if not( strcmp( data_location, 'card' ) )
+        data_location = args.Results.data_path;
+    end
+    [card_copy_success, raw_data_dir, mod_date] = ...
+        retrieve_tower_data_from_card( this_site, data_location );
+catch err
+    % echo the error message
+    fprintf( 'Error copying raw data from card to local drive.' )
+    disp( getReport( err ) );
+    main_success = 1;
+    % if copying the data was unsuccessful there is nothing to do, so return
+    diary off
+    return
 end
-[card_copy_success, raw_data_dir, mod_date] = ...
-    retrieve_tower_data_from_card( this_site, data_location );
 
 % convert the thirty-minute data to TOA5 file
-fprintf(1, '\n----------\n');
-fprintf(1, 'CONVERTING THIRTY-MINUTE DATA TO TOA5 FORMAT...\n');
-[fluxdata_convert_success, toa5_fname] = thirty_min_2_TOA5(this_site, ...
-                                                  raw_data_dir);
-fprintf(1, ' Done\n');
+try 
+    fprintf(1, '\n----------\n');
+    fprintf(1, 'CONVERTING THIRTY-MINUTE DATA TO TOA5 FORMAT...\n');
+    [fluxdata_convert_success, toa5_fname] = thirty_min_2_TOA5(this_site, ...
+                                                      raw_data_dir);
+    fprintf(1, ' Done\n');
+catch err
+    % echo the error message
+    fprintf( 'Error converting 30-minute data to TOA5 file.' )
+    disp( getReport( err ) );
+    main_success = fluxdata_convert_sucess;
+end
 
 %make diagnostic plots of the raw flux data from the card
 if args.Results.interactive
@@ -83,40 +112,81 @@ if args.Results.interactive
 end
 
 %convert the time series (10 hz) data to TOB1 files
-fprintf(1, '\n----------\n');
-fprintf(1, 'CONVERTING TIME SERIES DATA TO TOB1 FILES...');
-[tsdata_convert_success, ts_data_fnames] = ...
-    tsdata_2_TOB1(this_site, raw_data_dir);
-fprintf(1, ' Done\n');
+try 
+    fprintf(1, '\n----------\n');
+    fprintf(1, 'CONVERTING TIME SERIES DATA TO TOB1 FILES...');
+    [tsdata_convert_success, ts_data_fnames] = ...
+        tsdata_2_TOB1(this_site, raw_data_dir);
+    fprintf(1, ' Done\n');
+catch err
+    % echo the error report
+    fprintf( 'Error converting time series data to TOB1 files.' )
+    disp( getReport( err ) );
+    main_success = fluxdata_convert_sucess || tsdata_convert_sucess;
+    if not( main_sucess )
+        % if neither data file was converted successfully, exit
+        diary off
+        return
+    end
+end
 
 %copy uncompressed TOB1 data to MyBook
-fprintf(1, '\n----------\n');
-fprintf(1, 'COPYING UNCOMPRESSED TOB1 DATA TO MYBOOK...\n');
-copy_uncompressed_TOB_files(this_site, ts_data_fnames);
-fprintf(1, 'Done copying uncompressed TOB1 data to mybook\n');
+try 
+    fprintf(1, '\n----------\n');
+    fprintf(1, 'COPYING UNCOMPRESSED TOB1 DATA TO MYBOOK...\n');
+    copy_uncompressed_TOB_files(this_site, ts_data_fnames);
+    fprintf(1, 'Done copying uncompressed TOB1 data to mybook\n');
+catch err
+    % echo the error report
+    fprintf( 'Error copying uncompressed TOB1 data to MyBook\n' );
+    disp( getReport( err ) );
+    fprintf( 'continuing with processing\n' );
+end
 
 %copy uncompressed raw data to Story
-fprintf(1, '\n----------\n');
-fprintf(1, 'COPYING UNCOMPRESSED RAW CARD DATA TO STORY...\n');
-copy_uncompressed_raw_card_data(this_site, raw_data_dir);
-fprintf(1, 'Done copying uncompressed TOB1 data to mybook\n');
+try
+    fprintf(1, '\n----------\n');
+    fprintf(1, 'COPYING UNCOMPRESSED RAW CARD DATA TO STORY...\n');
+    copy_uncompressed_raw_card_data(this_site, raw_data_dir);
+    fprintf(1, 'Done copying uncompressed TOB1 data to mybook\n');
+catch err
+    % echo the error report
+    fprintf( 'Error copying uncompressed raw card data to Story\n' );
+    disp( getReport( err ) );
+    fprintf( 'continuing with processing\n' );
+end
 
 %compress the raw data on the local drive
-fprintf(1, '\n----------\n');
-fprintf(1, 'COMPRESSING RAW DATA ON INTERNAL DRIVE...\n');
-compress_raw_data_directory(raw_data_dir);
-fprintf(1, 'Done compressing\n');
+try
+    fprintf(1, '\n----------\n');
+    fprintf(1, 'COMPRESSING RAW DATA ON INTERNAL DRIVE...\n');
+    compress_raw_data_directory(raw_data_dir);
+    fprintf(1, 'Done compressing\n');
+catch err
+    % echo the error report
+    fprintf( 'Error compressing raw data\n' );
+    disp( getReport( err ) );
+    fprintf( 'continuing with processing\n' );
+end
 
 % transfer the compressed raw data to edac
-fprintf(1, '\n----------\n');
-if args.Results.interactive
-    fprintf(1, 'transfering compressed raw data to edac...\n');
-    h = msgbox( 'click to begin FTP transfer', '' );
-    waitfor( h );
-    transfer_2_edac(this_site, sprintf('%s.7z', raw_data_dir))
-    fprintf(1, 'Done transferring.\n');
-else
-    fprintf(1, 'Non-interactive -- skipping compressed raw data transfer to edac...\n');
+try
+    fprintf(1, '\n----------\n');
+    if args.Results.interactive
+        fprintf(1, 'transfering compressed raw data to edac...\n');
+        h = msgbox( 'click to begin FTP transfer', '' );
+        waitfor( h );
+        transfer_2_edac(this_site, sprintf('%s.7z', raw_data_dir))
+        fprintf(1, 'Done transferring.\n');
+    else
+        fprintf(1, ['Non-interactive -- skipping compressed raw data ' ...
+                    'transfer to edac...\n']);
+    end
+catch err
+    % echo the error report
+    fprintf( 'Error transfering compressed raw data to edac\n' );
+    disp( getReport( err ) );
+    fprintf( 'continuing with processing\n' );
 end
 
 save( 'card_restart_01.mat' );
@@ -125,13 +195,23 @@ save( 'card_restart_01.mat' );
 % the data are now copied from the card and backed up.
 
 % merge the new data into the fluxall file
-fprintf(1, '\n----------\n');
-fprintf(1, 'merging new data into FLUXALL file...\n');
-dates = cellfun( @get_TOA5_TOB1_file_date, ts_data_fnames );
-cdp = card_data_processor( UNM_sites( this_site ), ...
-                           'date_start', min( dates ), ...
-                           'date_end', max( dates ) + 1 );
-cdp.update_fluxall();
+try
+    fprintf(1, '\n----------\n');
+    fprintf(1, 'merging new data into FLUXALL file...\n');
+    dates = cellfun( @get_TOA5_TOB1_file_date, ts_data_fnames );
+    cdp = card_data_processor( UNM_sites( this_site ), ...
+                               'date_start', min( dates ), ...
+                               'date_end', max( dates ) + 1 );
+    cdp.update_fluxall();
+catch err
+    % echo the error report
+    fprintf( 'Error merging the new data into FLUXALL\n' );
+    disp( getReport( err ) );
+    main_success = 1;
+    % if fluxall was not updated successfully, there is nothing else to do.
+    diary off
+    return
+end
 
 % run RemoveBadData to create for gapfilling file, qc file.  
 fprintf(1, '\n----------\n');
@@ -158,3 +238,5 @@ fprintf(1, 'starting UNM_RemoveBadData...\n');
 [ year, ~, ~, ~, ~, ~ ] = datevec( min( dates ) );
 UNM_RemoveBadData( UNM_sites( this_site ), year, 'draw_plots', true );
 
+% close the log file
+diary off
