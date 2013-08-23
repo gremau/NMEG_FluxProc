02a3d2d372fa08da554e99054843c4cb13e57894
Timothy W. Hilton <hilton@unm.edu>
2013-08-22 17:01:20 -0700

    added a 'reference' parameter to plot_monthly_aggregated_daily_cycle to allow, for example, solar angle to be plotted alongside the observed data

--- plot_monthly_aggregated_daily_cycle.m
+++ plot_monthly_aggregated_daily_cycle.m
@@ -6,25 +6,38 @@ function plot_monthly_aggregated_daily_cycle( mm, varargin )
 % for a month.
 %   
 % INPUTS
-%    mm: dataset with variables year, month, hour, and val, with val the
+%    mm: Nx4 dataset array with variables year, month, hour, and val, with val the
 %        aggregated value to be plotted.  mm is usually the output of
 %        monthly_aggregated_daily_cycle.
 %
 % PARAMETER-VALUE PAIRS
-%    main_title: string, optional; title to appear above entire plot
-%    figure_file_name: string, optional; specifies full path for plot to be saved to an
-%        encapsulated postscript (eps) file.  If not specified plot is not
-%        saved. 
+%    main_title: string, optional; title to appear above entire plot.  If not
+%        specified the plot title is blank.
+%    figure_file_name: string, optional; specifies full path for plot to be
+%        saved to an encapsulated postscript (eps) file.  If not specified plot
+%        is not saved.
+%    ref_vals: N-element vector; optional reference values to plot alongside
+%       the monthly cycle.  If specified, they are plotted as a black line.
+%       Useful for plotting calculated solar angle or top-of-atmosphere
+%       potential radiation alongside observed radiation.
 %
 % OUTPUTS
 %    none
 %
+% SEE ALSO
+%    dataset, monthly_aggregated_daily_cycle
+%
 % author: Timothy W. Hilton, UNM, May 2013
+
+% define some constants
+HOURS_PER_DAY = 24;
+MONTHS_PER_YEAR = 12;
 
 args = inputParser;
 args.addRequired( 'mm', @(x) isa( x, 'dataset' ) );
 args.addParamValue( 'main_title', '', @ischar );
 args.addParamValue( 'figure_file_name', '', @ischar );
+args.addParamValue( 'ref_vals', [], @isnumeric );
 args.parse( mm, varargin{ : } );
 mm = args.Results.mm;
 
@@ -42,11 +55,31 @@ h_fig = figure( 'NumberTitle', 'Off', ...
                 'DefaultAxesColorOrder', pal );
 
 y_max = nanmax( mm.val );
-vals = reshape( mm.val, 24, 12, [] );
+vals = reshape( mm.val, HOURS_PER_DAY, MONTHS_PER_YEAR, [] );
+
+if not( isempty( args.Results.ref_vals ) )
+    ref_vals = reshape( args.Results.ref_vals, ...
+                        HOURS_PER_DAY, ...
+                        MONTHS_PER_YEAR, ...
+                        [] );
+end
+
 for this_month = 1:12
     h_ax = subplot( 3, 4, this_month );
-    h_lines = plot( squeeze( vals( :, this_month, : ) ), '-o',...
-                    'MarkerSize', 8 );
+    this_data = squeeze( vals( :, this_month, : ) );
+    h_lines = plot( this_data, '-o', 'MarkerSize', 8 );
+    
+   if not( isempty( args.Results.ref_vals ) )
+       % if reference values were provided, add them to the plot as a black line
+       hold on;
+       this_ref_vals = squeeze( ref_vals( :, this_month, : ) );
+       this_ref_vals( this_ref_vals < 0.0 ) = 0.0;
+       maxval = nanmax( reshape( this_data, 1, [] ) );
+       this_ref_vals = normalize_vector( this_ref_vals, 0, maxval);
+       h_ref = plot( this_ref_vals, '*k' );
+       hold off
+   end 
+    
     % set axis limits
     ylim( [ 0, y_max ] );
     xlim( [ 1, 24 ] );
@@ -61,7 +94,14 @@ for this_month = 1:12
 end
 
 % legend and main title
-h_leg = legend( h_lines, num2str( unique( mm.year ) ) );
+leg_text = arrayfun( @num2str, ...
+                     unique( mm.year ), ...
+                     'UniformOutput', false );
+if not( isempty( args.Results.ref_vals ) )
+    leg_text = [ leg_text; 'reference' ];
+    h_lines = [ h_lines; h_ref ];
+end
+h_leg = legend( h_lines, leg_text );
 set( h_leg, 'Units', 'Normalized', 'Position', [ 0.9, 0.4, 0.1, 0.2 ] );
 
 if not( isempty( args.Results.main_title ) )
