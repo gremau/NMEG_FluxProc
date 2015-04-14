e60a97692e691759cf866d62813c7fd38f4c9ce1
Gregory E. Maurer <gregmaurer@unm.edu>
2015-04-14 15:44:32 -0600

    Small change to ease making multiyear daily ameriflux files

--- scripts/script_make_ameriflux.m
+++ scripts/script_make_ameriflux.m
@@ -1,15 +1,16 @@
 %close all;
 %clear all;
 %
-sitelist = { UNM_sites.PPine, UNM_sites.MCon, ...
-    UNM_sites.JSav, UNM_sites.PJ, UNM_sites.PJ_girdle ...
-     UNM_sites.SLand, UNM_sites.GLand, UNM_sites.New_GLand};
-%sitelist = {UNM_sites.GLand};
+% sitelist = { UNM_sites.PPine, UNM_sites.MCon, ...
+%     UNM_sites.JSav, UNM_sites.PJ, UNM_sites.PJ_girdle ...
+%      UNM_sites.SLand, UNM_sites.GLand, UNM_sites.New_GLand};
+sitelist = {UNM_sites.GLand};
 %sitelist = {UNM_sites.PJ_girdle};
 %sitelist = {UNM_sites.New_GLand};
 
-yearlist = 2010:2014;
+yearlist = 2009:2014;
 partmethod = 'eddyproc';%'Reddyproc'
+make_daily = false;
 
 for i = 1:length(sitelist);
     close all;
@@ -21,7 +22,7 @@ for i = 1:length(sitelist);
         if strcmp(partmethod, 'eddyproc');
         
             UNM_Ameriflux_File_Maker_TWH(sitecode, year,...
-                'write_daily_file', false, 'process_soil_data', false);
+                'write_daily_file', true, 'process_soil_data', false);
             
         elseif strcmp(partmethod, 'Reddyproc');
             
@@ -35,6 +36,15 @@ for i = 1:length(sitelist);
         %    fullfile(getenv('FLUXROOT'), 'FluxOut' ));
         %clear year;
     end
+    
+    % Make daily files? All (desired) AF files should be in
+    % $FLUXROOT$/Ameriflux_files
+    if make_daily
+        agg = UNM_Ameriflux_daily_aggregator(sitecode);
+        agg.write_daily_file();
+    end
+    
+    
     clear sitecode;
 end
 
