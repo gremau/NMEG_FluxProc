3fe39e8846be64e2cd2ad701d14cd74722a9dc11
Gregory E. Maurer <gregmaurer@unm.edu>
2015-11-30 01:12:04 -0700

    Adding a couple untracked files

---
+++ scripts/script_make_soilmet_qc.m
@@ -0,0 +1,24 @@
+%close all;
+%clear all;
+
+sitelist = {UNM_sites.GLand, UNM_sites.SLand, ...
+    UNM_sites.JSav, UNM_sites.PJ, UNM_sites.PPine, ...
+    UNM_sites.MCon};
+sitelist = { UNM_sites.PJ };%, ...
+    %UNM_sites.SLand};
+yearlist = 2008:2014;%2013:2014;% 2009:2013;
+write_qc = true;
+write_rbd = true;
+
+count = 1;
+for i = 1:length(sitelist);
+    for j = 1:length(yearlist)
+        % Set site and year
+        sitecode = sitelist{i};
+        year = yearlist(j);
+        soil_corr = soil_met_correct( sitecode, year, write_qc, write_rbd );
+        
+        count = count + 1;
+        close all;
+    end
+end
