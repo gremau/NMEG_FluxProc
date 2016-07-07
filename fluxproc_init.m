9da4133a34be33c63fa233242514e872f24f00da
Gregory E. Maurer <gregmaurer@unm.edu>
2016-07-07 13:40:35 -0600

    Fix paths

--- fluxproc_init.m
+++ fluxproc_init.m
@@ -1,14 +1,11 @@
-addpath('scripts/')
-addpath('m_utils/')
-addpath('m_exchange_utils/')
-addpath('retrieve_card_data')
+addpath( fullfile( pwd, 'scripts' ))
+addpath( fullfile( pwd, 'm_utils' ))
+addpath( genpath( fullfile( pwd, 'm_exchange_utils' )))
+addpath( fullfile( pwd, 'retrieve_card_data' ))
 %addpath('Utilities/') - moved to utils/
-addpath('plots/')
+addpath( fullfile( pwd, 'plots' ))
 
 % Moved to ./m_utils/
 %addpath('/home/greg/current/MatlabFluxUtilities/')
 % Moved to ./m_exchange_utils/
 %addpath('/home/greg/current/MatlabGeneralUtilities/')
-
-addpath('m_exchange_utils/YAMLMatlab_0.4.3/')
-addpath('m_exchange_utils/altmany-export_fig-76bd7fa/')
