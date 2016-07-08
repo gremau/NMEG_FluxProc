e3288d351477b06bb4dc0ae8a5ffd38b8ffd259c
Gregory E. Maurer <gregmaurer@unm.edu>
2016-07-08 10:04:18 -0600

    path setting changes

---
+++ fluxproc_setpaths.m
@@ -0,0 +1,17 @@
+% Sets paths in fluxproc directory
+if ~isempty( regexp( pwd, 'FluxProc' ) )
+    fprintf( 'Setting paths for FluxProc...\n' );
+    addpath( fullfile( pwd, 'scripts' ))
+    addpath( fullfile( pwd, 'm_utils' ))
+    addpath( genpath( fullfile( pwd, 'm_exchange_utils' )))
+    addpath( fullfile( pwd, 'retrieve_card_data' ))
+    %addpath('Utilities/') - moved to utils/
+    addpath( fullfile( pwd, 'plots' ))
+else
+    error( 'Present directory not a FluxProc directory!' );
+end
+
+% Moved to ./m_utils/
+%addpath('/home/greg/current/MatlabFluxUtilities/')
+% Moved to ./m_exchange_utils/
+%addpath('/home/greg/current/MatlabGeneralUtilities/')
